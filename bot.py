b93733ed3afb8d60b67ed4a7f0f683536dee234a
ogdoremon <surajtandle703@gmail.com>
2026-02-28 21:10:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,27 @@
+
+        
 import os
-import asyncio
 from pyrogram import Client, idle
 
-# Render settings se data uthana
-api_id = int(os.environ.get("API_ID"))
-api_hash = os.environ.get("API_HASH")
-string_session = os.environ.get("STRING_SESSION")
+# Render settings se details uthana
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+STRING_SESSION = os.environ.get("STRING_SESSION")
 
-async def start_bot():
-    app = Client(
-        "suraj_userbot",
-        session_string=string_session,
-        api_id=api_id,
-        api_hash=api_hash
-    )
-    
-    await app.start()
-    print("Bhai, Tera Userbot 24/7 Chalu Ho Gaya Hai! 🚀")
-    await idle()
-    await app.stop()
+# Client setup
+app = Client(
+    "suraj_userbot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=STRING_SESSION
+)
+
+def start_bot():
+    app.start()
+    print("Bhai, Tera Userbot Ab 24/7 Live Hai! 🚀")
+    idle()
+    app.stop()
 
 if __name__ == "__main__":
-    # Naye Python versions ke liye sabse sahi tarika
-    try:
-        asyncio.run(start_bot())
-    except (KeyboardInterrupt, SystemExit):
-        pass
-        
+    start_bot()
+    
